af8b84108459235bc397abdba08791ba09c14d4f
MarsMeow <39848685+MarsMeow@users.noreply.github.com>
2023-04-11 16:59:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,17 +8,17 @@
 const USER_CONFIG = {
   USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx637ad93b24d3a13e',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'cf003839ab6876b94f5a1fa47f7e842d',
+  APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '',
+  CITY: '',
 
   USERS: [
     {
-      // 想要发送的人的名字
+      // 想要发送的人的名字1
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU21798ThDHjFct3iGC8Zw5EV7oaKR8vG8eDhx7m',
@@ -46,21 +46,39 @@ const USER_CONFIG = {
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
+      
+      {
+      // 想要发送的人的名字2
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'PDU21800TgJODvUfgl3hDMhqNSbLOQuH0UeZMYgR9',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0002',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '李四', year: '1996', date: '09-31',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
     },
   ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'PDU21800TgJODvUfgl3hDMhqNSbLOQuH0UeZMYgR9',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '0002',
-    }
-  ],
-
 }
 
 module.exports = USER_CONFIG
